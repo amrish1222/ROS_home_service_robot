992b00b4e2c0530cfe3dfe6d55866c968c043dfb
amrish1222 <amrish1222@gmail.com>
2020-05-25 23:05:33 +0000

    updating rviz config for add markers

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -3,6 +3,6 @@ xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/w
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/map/my_world.yaml " &
 sleep 5
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+xterm -e " rosrun rviz rviz -d /home/workspace/catkin_ws/src/rvizConfig/home_service.rviz " &
 sleep 15
 xterm -e "rosrun add_markers add_markers"
