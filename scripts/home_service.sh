cf648b3f17a0800ff0c4d9e3de5963f7500dd373
amrish1222 <amrish1222@gmail.com>
2020-05-25 22:52:55 +0000

    initial commit

---
+++ scripts/home_service.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/my_world.world " &
+sleep 5
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/map/my_world.yaml " &
+sleep 5
+xterm -e " rosrun rviz rviz -d /home/workspace/catkin_ws/src/rvizConfig/home_service.rviz" &
+sleep 15
+xterm -e "rosrun add_markers add_markers " &
+sleep 5
+xterm -e "rosrun pick_objects pick_objects"
